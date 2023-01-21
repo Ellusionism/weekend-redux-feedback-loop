b0dd8bf2120df6b04f574c6fc02bad012b1d0af6
Mike Schwartz <michael.schwartz001@gmail.com>
2023-01-20 22:25:54 -0600

    added reducers, store, provider in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import logger from 'redux-logger';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Reducer for submitting feeling rating
+const feeling = (state = 0, action) => {
+  if (action.type === 'SUBMIT_FEELING') {
+    return action.payload;
+  };
+  return state;
+};
+
+// Reducer for submitting understanding rating
+const understanding = (state = 0, action) => {
+  if (action.type === 'SUBMIT_UNDERSTANDING') {
+    return action.payload;
+  };
+  return state;
+};
+
+// Reducer for submitting support rating
+const support = (state = 0, action) => {
+  if (action.type === 'SUBMIT_SUPPORT') {
+    return action.payload;
+  };
+  return state;
+};
+
+// Reducer for submitting comments
+const comments = (state = '', action) => {
+  if (action.type === 'SUBMIT_COMMENTS') {
+    return action.payload;
+  };
+  return state;
+};
+
+const store = createStore(
+  combineReducers({
+    feeling,
+    understanding,
+    support,
+    comments
+  }),
+  applyMiddleware(
+    logger
+  )
+)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 registerServiceWorker();
