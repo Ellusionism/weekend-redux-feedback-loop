2763be179cfd73d239128a619a863eea9d5b8aa3
Mike Schwartz <michael.schwartz001@gmail.com>
2023-01-22 02:11:25 -0600

    added router and routes to App.js

---
+++ server/routes/feedbackRouter.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool.js');
+
+router.post("/", (req, res) => {
+  const feedback = {
+    feeling: req.body.feeling,
+    understanding: req.body.understanding,
+    support: req.body.support,
+    comments: req.body.comments,
+  };
+  sqlQuery = `
+    INSERT INTO prime_feedback ("feeling", "understanding", "support", "comments")
+    VALUES ($1, $2, $3, $4)
+    `
+  sqlValues = [feedback.feeling, feedback.understanding, feedback.support, feedback.comments];
+  pool.query(sqlQuery, sqlValues)
+  .then((resp) => {
+    res.sendStatus(201);
+  }).catch((err) => {
+    console.error(`Error in feedbackRouter POST: ${err}`)
+    res.sendStatus(500);
+  });
+});
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
-
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Feeling from '../Feeling/Feeling.js';
+import Understanding from '../Understanding/Understanding.js';
+import Support from '../Support/Support.js';
+import Comments from '../Comments/Comments.js';
 function App() {
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+        <Route exact path = '/'>
+          <Home />
+        </Route>
+        <Route exact path = '/feeling'>
+          <Feeling />
+        </Route>
+        <Route exact path = '/understanding'>
+          <Understanding />
+        </Route>
+        <Route exact path = '/support'>
+          <Support />
+        </Route>
+        <Route exact path = '/comments'>
+          <Comments />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
