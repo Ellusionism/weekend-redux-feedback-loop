612deedf3847d02c8ec7e9e3beb15c71c3d861e6
Mike Schwartz <michael.schwartz001@gmail.com>
2023-01-22 02:45:55 -0600

    added Understanding and Support components

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -1,7 +1,38 @@
-
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
 function Support() {
-  return;
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [support, setSupport] = useState(0);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    submitSupport();
+};
+
+  const submitSupport = () => {
+    dispatch({
+      type: 'SUBMIT_SUPPORT',
+      payload: support
+    })
+    history.push('/comments')
+  }
+
+  return(
+    <form onSubmit = {handleSubmit}>
+      <p>How well were you being supported today?</p>
+      <input
+        value = {support}
+        type = 'number'
+        min = '1'
+        max = '5'
+        onChange = {(event) => setSupport(event.target.value)}
+        required />
+        <button type = 'submit'>NEXT</button>
+    </form>
+  );
 };
 
 export default Support;
--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -1,7 +1,38 @@
-
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
 function Understanding() {
-  return;
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [understanding, setUnderstanding] = useState(0);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    submitUnderstanding();
+};
+
+  const submitUnderstanding = () => {
+    dispatch({
+      type: 'SUBMIT_UNDERSTANDING',
+      payload: understanding
+    })
+    history.push('/support')
+  }
+
+  return(
+    <form onSubmit = {handleSubmit}>
+      <p>How well are you understanding today's content?</p>
+      <input
+        value = {understanding}
+        type = 'number'
+        min = '1'
+        max = '5'
+        onChange = {(event) => setUnderstanding(event.target.value)}
+        required />
+        <button type = 'submit'>NEXT</button>
+    </form>
+  );
 };
 
 export default Understanding;
